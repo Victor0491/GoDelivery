394bf4b56f26ce15356b8bee3804e3a72564bbc3
JorgeFuentes13 <jo.fuentess@duocuc.cl>
2023-10-16 00:08:02 -0400

    Conexion a firebase parte1

--- Delivery/src/environments/environment.prod.ts
+++ Delivery/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyApknUfpy0VdjTQPX3AnauSHaf_sEZ8Oek",
+    authDomain: "godelivery-53b73.firebaseapp.com",
+    projectId: "godelivery-53b73",
+    storageBucket: "godelivery-53b73.appspot.com",
+    messagingSenderId: "188173833696",
+    appId: "1:188173833696:web:0132818fd24d085f970785",
+    measurementId: "G-6180NENJ04"
+  }
 };
--- Delivery/src/environments/environment.ts
+++ Delivery/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyApknUfpy0VdjTQPX3AnauSHaf_sEZ8Oek",
+    authDomain: "godelivery-53b73.firebaseapp.com",
+    projectId: "godelivery-53b73",
+    storageBucket: "godelivery-53b73.appspot.com",
+    messagingSenderId: "188173833696",
+    appId: "1:188173833696:web:0132818fd24d085f970785",
+    measurementId: "G-6180NENJ04"
+  }
 };
 
 /*
