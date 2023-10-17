05fd42281787124572ad8819fad974fb163e0aed
Jordan Soto <jord.sotol@duocuc.cl>
2023-10-17 10:31:36 -0600

    Creacion page e404 y cambion en variables de colores css por defecto (medium color)

--- Delivery/src/app/app-routing.module.ts
+++ Delivery/src/app/app-routing.module.ts
@@ -2,12 +2,28 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
+  {
+    path: 'inicio',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'e404',
+    pathMatch: 'full'
+  },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'e404',
+    loadChildren: () => import('./e404/e404.module').then( m => m.E404PageModule)
   },
 
 ];
